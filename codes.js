f0cae0f036f9ee1d20b105f381e96886ffb0cb3c
UC Hacker <uc_hacker@mail.uc.edu>
2016-08-25 18:59:34 -0400

    Add Button to display

--- codes.js
+++ codes.js
@@ -1,5 +1,8 @@
 function button() {
 	var number1 = document.getElementById('Number1').value;
+        var message = “Congrats on learning git at the ACM meeting!”
+	document.getElementById('MessageBox').innerText = message;
+
   	var number2 = document.getElementById('Number2').value;
 	var out = parseFloat(number1) * parseFloat(number2);
 	document.getElementById('output').innerText = out;
